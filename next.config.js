fa0b235acd59ba60dbf5d7a5866719a5af8c1547
showMaker <946519656@qq.com>
2025-05-22 13:15:21 +0800

    Fix: ignore optional ws dependencies

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'bufferutil': 'commonjs bufferutil',
+        'utf-8-validate': 'commonjs utf-8-validate',
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
